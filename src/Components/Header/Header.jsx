98c0bdf1b265c41a6640ebf272b3a5f48a215634
umair2607 <umair.k2607@gmail.com>
2022-08-13 08:47:00 +0500

    completed header section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,11 @@
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from "../HeaderOption/HeaderOption.jsx";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 const Header = () => {
   return (
@@ -14,7 +20,17 @@ const Header = () => {
           <input type="text" />
         </div>
       </div>
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://avatars.githubusercontent.com/u/81556052?v=4"
+          title="me"
+        />
+      </div>
     </div>
   );
 };
