0fc8f5d77182078b3dcb4e1b6adb01fecdc0c21f
umair2607 <umair.k2607@gmail.com>
2022-08-14 15:02:57 +0500

    impleted basic redux structure

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import "./App.css";
 import Header from "./Components/Header/Header";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Feed from "./Components/Feed/Feed";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
       <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* Widgets */}
-      </div>
+      {!user ? (
+        <h1>Login</h1>
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          {/* Widgets */}
+        </div>
+      )}
     </div>
   );
 }
